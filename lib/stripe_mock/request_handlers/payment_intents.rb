ee9a7a7c25c99536d8a14c2365a145dbc1421b52
shigeek <naoki13futsal@gmail.com>
2023-03-30 18:56:16 +0900

    Revert "tweak"
    This reverts commit 63421aec6f45f1ccafca39eba132c1d59c804d45.

--- lib/stripe_mock/request_handlers/payment_intents.rb
+++ lib/stripe_mock/request_handlers/payment_intents.rb
@@ -173,12 +173,12 @@ module StripeMock
         payment_intent[:status] = 'succeeded'
         btxn = new_balance_transaction('txn', { source: payment_intent[:id] })
 
-        # payment_intent[:charges][:data] << Data.mock_charge(
-        #   balance_transaction: btxn,
-        #   amount: payment_intent[:amount],
-        #   currency: payment_intent[:currency],
-        #   payment_method: payment_intent[:payment_method]
-        # )
+        payment_intent[:charges][:data] << Data.mock_charge(
+          balance_transaction: btxn,
+          amount: payment_intent[:amount],
+          currency: payment_intent[:currency],
+          payment_method: payment_intent[:payment_method]
+        )
 
         payment_intent
       end
